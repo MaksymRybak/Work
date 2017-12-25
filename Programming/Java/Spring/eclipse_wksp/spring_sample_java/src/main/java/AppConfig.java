77b38e369593a26e5f3b94f655cd9293e9d3c58d
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 14:23:08 +0100

    added project using maven java configuration (setter injection)

---
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/AppConfig.java
@@ -0,0 +1,24 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.pluralsight.repository.CustomerRepository;
+import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import com.pluralsight.service.CustomerService;
+import com.pluralsight.service.CustomerServiceImpl;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean(name = "customerService")
+	public CustomerService getCustomerService() {
+		CustomerServiceImpl customerService = new CustomerServiceImpl();
+		customerService.setCustomerRepository(getCustomerRepository());
+		return customerService;
+	}
+	
+	@Bean(name = "customerRepository")
+	public CustomerRepository getCustomerRepository() {
+		return new HibernateCustomerRepositoryImpl();
+	}
+	
+}
---
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/Application.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.pluralsight.service.CustomerService;
+import com.pluralsight.service.CustomerServiceImpl;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		//CustomerService service = new CustomerServiceImpl();
+		
+		CustomerService customerService = appContext.getBean("customerService", CustomerService.class);
+		
+		System.out.println(customerService.findAll().get(0).getFirstName());
+		
+	}
+
+}
