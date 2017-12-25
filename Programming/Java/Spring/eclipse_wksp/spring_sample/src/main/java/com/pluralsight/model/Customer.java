4d515b87c83f40b62e00ffd555a36a81112eee46
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 10:07:48 +0100

    added model classes

---
+++ Programming/Java/Spring/eclipse_wksp/spring_sample/src/main/java/com/pluralsight/model/Customer.java
@@ -0,0 +1,28 @@
+package com.pluralsight.model;
+
+public class Customer {
+	
+	private String firstName;
+	private String lastName;
+
+	public Customer() {
+		
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
