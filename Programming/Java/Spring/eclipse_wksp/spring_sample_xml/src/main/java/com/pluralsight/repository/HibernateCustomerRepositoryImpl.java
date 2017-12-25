b132ba61a1d9de1a3516d1d7392679fced6ee3fb
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 15:38:15 +0100

    add properties file and use context property configuration to read it's values

--- Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -7,8 +7,16 @@ import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
+    private String dbUsername;
+	
+	public void setDbUsername(String dbUsername) {
+		this.dbUsername = dbUsername;
+	}
+
 	@Override
 	public List<Customer> findAll(){
+        System.out.println(this.dbUsername);
+
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
