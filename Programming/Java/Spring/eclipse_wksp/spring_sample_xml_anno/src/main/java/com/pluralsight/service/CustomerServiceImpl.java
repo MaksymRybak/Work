889972fe52d9b48398d1d11e1b1168a46d6af26e
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 13:57:12 +0100

    setter and constructor injection

--- Programming/Java/Spring/eclipse_wksp/spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -11,12 +11,24 @@ import com.pluralsight.repository.CustomerRepository;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-	@Autowired
+	//@Autowired
 	private CustomerRepository customerRepository;
+	
+	@Autowired
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		System.out.println("We are using constructor injection");
+		this.customerRepository = customerRepository;
+	}
 	
 	@Override
 	public List<Customer> findAll(){
 		return customerRepository.findAll();
+	}
+
+	//@Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		System.out.println("We are using setter injection");
+		this.customerRepository = customerRepository;
 	}
 	
 }
