8c4874cb4c83cff5d0516b2158bc7cf6d895b13b
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 11:21:54 +0100

    try tu use constructor injection

--- Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -7,9 +7,6 @@ import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
-	/* (non-Javadoc)
-	 * @see com.pluralsight.repository.CustomerRepository#findAll()
-	 */
 	@Override
 	public List<Customer> findAll(){
 		List<Customer> customers = new ArrayList<>();
--- Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -9,6 +9,15 @@ public class CustomerServiceImpl implements CustomerService {
 
 	private CustomerRepository customerRepository;
 	
+	
+	public CustomerServiceImpl() {
+		
+	}
+	
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
 	@Override
 	public List<Customer> findAll(){
 		return customerRepository.findAll();
