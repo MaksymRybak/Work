492e244c74ba80fb2635477188a0b82751c63457
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 16:49:20 +0100

    create spring starter web project

---
+++ Programming/Java/Spring MVC4/eclipse_wksp/EventTracker/src/main/java/com/pluralsight/EventTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTrackerApplication.class, args);
+	}
+}
