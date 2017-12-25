510e17bb356525057d64c9e0ffb4ac8d31173034
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 15:12:05 +0100

    use singleton scope in spring xml configuration

--- Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/Application.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/Application.java
@@ -12,6 +12,10 @@ public class Application {
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		CustomerService service = appContext.getBean("customerService", CustomerService.class);
+		System.out.println(service);
+		
+		CustomerService service2 = appContext.getBean("customerService", CustomerService.class);
+		System.out.println(service2);
 		
 		System.out.println(service.findAll().get(0).getFirstName());
 		
