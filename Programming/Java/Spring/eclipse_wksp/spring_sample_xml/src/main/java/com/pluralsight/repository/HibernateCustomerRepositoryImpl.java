b9e76a54bdec092cfe9c316007b859873c4ad270
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 15:43:00 +0100

    read properties file using annotation @Value

--- Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,15 +3,18 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
+	@Value("${dbUsername}")
     private String dbUsername;
 	
-	public void setDbUsername(String dbUsername) {
+	/*public void setDbUsername(String dbUsername) {
 		this.dbUsername = dbUsername;
-	}
+	}*/
 
 	@Override
 	public List<Customer> findAll(){
