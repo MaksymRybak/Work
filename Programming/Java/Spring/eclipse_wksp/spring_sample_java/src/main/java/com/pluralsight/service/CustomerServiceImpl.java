cf02ce91b77dbc4addf8849600eed3985ec3322a
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 15:08:13 +0100

    use SCOPE_SINGLETON enum value

--- Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.pluralsight.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,7 @@ import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 
 @Service("customerService")
-@Scope("singleton")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
