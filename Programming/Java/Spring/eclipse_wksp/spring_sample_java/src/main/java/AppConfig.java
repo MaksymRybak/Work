05ea091cd5e2e3414775cabc7926363dc10badbe
MaksymRybak <rybak.maksym@gmail.com>
2017-12-25 15:52:56 +0100

    read properties file using java configuration method

--- Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/AppConfig.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/AppConfig.java
@@ -1,13 +1,18 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties")
 public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	/*@Bean(name = "customerService")
 	public CustomerService getCustomerService() {
--- Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ Programming/Java/Spring/eclipse_wksp/spring_sample_java/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Customer;
@@ -10,8 +11,13 @@ import com.pluralsight.model.Customer;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
+	@Value("${dbUsername}")
+	private String dbUsername;
+	
 	@Override
 	public List<Customer> findAll(){
+		System.out.println(dbUsername);
+		
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
