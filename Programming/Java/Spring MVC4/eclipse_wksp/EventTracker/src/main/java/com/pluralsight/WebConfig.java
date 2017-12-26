bab1e9a5e054029e72ae25248754df0718312e47
MaksymRybak <rybak.maksym@gmail.com>
2017-12-26 09:35:14 +0100

    config spring controller using web.xml

--- Programming/Java/Spring MVC4/eclipse_wksp/EventTracker/src/main/java/com/pluralsight/WebConfig.java
+++ Programming/Java/Spring MVC4/eclipse_wksp/EventTracker/src/main/java/com/pluralsight/WebConfig.java
@@ -1,10 +1,12 @@
 package com.pluralsight;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = "com.pluralsight")
 public class WebConfig {
 	
 	
