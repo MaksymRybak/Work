ceb8a300e7fa2329527625789d627fa3674512fc
MaksymRybak <rybak.maksym@gmail.com>
2017-12-26 08:29:00 +0100

    removed unused files and folders added index.jsp and empty WebConfig (will host spring configuration)

---
+++ Programming/Java/Spring MVC4/eclipse_wksp/EventTracker/src/main/java/com/pluralsight/WebConfig.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+	
+	
+	
+}
