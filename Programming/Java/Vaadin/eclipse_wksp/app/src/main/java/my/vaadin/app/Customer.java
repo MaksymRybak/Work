a36fcff582e2e14df325bf0169ce2fa0cdf0bb84
Maksym Rybak <rybak.maksym@gmail.com>
2018-01-23 21:49:29 +0100

    added based model classes

---
+++ Programming/Java/Vaadin/eclipse_wksp/app/src/main/java/my/vaadin/app/Customer.java
@@ -0,0 +1,102 @@
+package my.vaadin.app;
+
+import java.util.Date;
+
+public class Customer {
+	private Date birthDate;
+
+	private String email = "";
+
+	private String firstName = "";
+
+	private Long id;
+
+	private String lastName = "";
+
+	private CustomerStatus status;
+
+	@Override
+	public Customer clone() throws CloneNotSupportedException {
+		return (Customer) super.clone();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (this.id == null) {
+			return false;
+		}
+
+		if (obj instanceof Customer && obj.getClass().equals(getClass())) {
+			return this.id.equals(((Customer) obj).id);
+		}
+
+		return false;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public CustomerStatus getStatus() {
+		return status;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 43 * hash + (id == null ? 0 : id.hashCode());
+		return hash;
+	}
+
+	public boolean isPersisted() {
+		return id != null;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setStatus(CustomerStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return firstName + " " + lastName;
+	}
+}
