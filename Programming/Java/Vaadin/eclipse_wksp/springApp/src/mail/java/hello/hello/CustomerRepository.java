f68d9694b1e817343fcf80989efb61fae825d00d
Maksym Rybak <rybak.maksym@gmail.com>
2018-01-28 18:05:13 +0100

    Implemented necessary classes using spring boot

---
+++ Programming/Java/Vaadin/eclipse_wksp/springApp/src/mail/java/hello/hello/CustomerRepository.java
@@ -0,0 +1,10 @@
+package hello;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	List<Customer> findByLastNameStartsWithIgnoreCase(String lastName);
+}
---
+++ Programming/Java/Vaadin/eclipse_wksp/springApp/src/mail/java/hello/hello/VaadinUI.java
@@ -0,0 +1,84 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringUI
+@Theme("valo")
+public class VaadinUI extends UI {
+	CustomerRepository repo;
+	
+	private final CustomerEditor editor;
+	
+	Grid<Customer> grid;
+	
+	final TextField filter;
+
+	private final Button addNewBtn;
+	
+	@Autowired
+	public VaadinUI(CustomerRepository repo, CustomerEditor editor) {
+		this.repo = repo;
+		this.editor = editor;
+		this.grid = new Grid<>(Customer.class);
+		this.filter = new TextField();
+		this.addNewBtn = new Button("New customer", FontAwesome.PLUS);
+	}
+	
+	@Override
+	protected void init(VaadinRequest request) {
+		// build layout
+		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+		VerticalLayout mainLayout = new VerticalLayout(actions, grid, editor);
+		setContent(mainLayout);
+
+		grid.setHeight(300, Unit.PIXELS);
+		grid.setColumns("id", "firstName", "lastName");
+
+		filter.setPlaceholder("Filter by last name");
+
+		// Hook logic to components
+
+		// Replace listing with filtered content when user changes filter
+		filter.setValueChangeMode(ValueChangeMode.LAZY);
+		filter.addValueChangeListener(e -> listCustomers(e.getValue()));
+
+		// Connect selected Customer to editor or hide if none is selected
+		grid.asSingleSelect().addValueChangeListener(e -> {
+			editor.editCustomer(e.getValue());
+		});
+
+		// Instantiate and edit new Customer the new button is clicked
+		addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
+
+		// Listen changes made by the editor, refresh data from backend
+		editor.setChangeHandler(() -> {
+			editor.setVisible(false);
+			listCustomers(filter.getValue());
+		});
+
+		// Initialize listing
+		listCustomers(null);
+	}
+
+	void listCustomers(String filterText) {
+		if (StringUtils.isEmpty(filterText)) {
+			grid.setItems(repo.findAll());
+		}
+		else {
+			grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
+		}
+	}
+}
